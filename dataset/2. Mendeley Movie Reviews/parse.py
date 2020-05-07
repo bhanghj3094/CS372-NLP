14eed3b6e0291dc67b6e2f77326cca6b1861ed96
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-05-08 00:22:06 +0900

    feat: add mendeley movie reviews

---
+++ dataset/2. Mendeley Movie Reviews/parse.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, json, gzip
+import pandas as pd
+import pprint
+
+# Movie Dataset
+datasets = [
+    "2012",
+    "A Beautiful Mind",
+    "Amadeus",
+    "Avatar",
+    "Clash of the Titans",
+    "Les Miserables",
+    "Star Wars Episode I - The Phantom Menace",
+    "The Expendables I",
+    "The Godfather",
+    "The Matrix Revolutions",
+]
+
+for index, dataset in enumerate(datasets):
+    # Open ratings
+    file = open(dataset + "/rating.txt", "r")
+    ratings = file.readline().strip()
+    ratings = ratings[len("rating = "):].replace(", ", "")
+    ratings = ratings[2:-2].split()
+    file.close()
+
+    # Add reviews for each rating
+    reviews = []
+    for i in range(len(ratings)):
+        file = open(dataset + "/" + str(i+1) + ".txt", "r")
+        review = " ".join(file.read().strip().split("\n"))
+        reviews.append((review, ratings[i]))
+        file.close()
+
+    # Save into csv
+    name = "output/" + dataset + "(" + str(len(reviews)) + ").csv"
+    output = open(name, "w")
+    for review, rating in reviews:
+        output.write(",".join([review, rating]) + "\n")
+    output.close()
