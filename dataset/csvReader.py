d364d6d53fb22b2693415668674c6f97a6ecdf61
mjhbest <mjhbest@kaist.ac.kr>
2020-05-08 01:49:21 +0900

    feat : change csvReader instance, add instructions

--- dataset/csvReader.py
+++ dataset/csvReader.py
@@ -3,25 +3,29 @@ import csv
 
 class Reader():
 	def __init__(self):
-		self.directory = os.getcwd()
+		self.directory = os.getcwd() #code directory, 기본 설정 :  가장 바깥으로
 		self.folder = ''
 		self.file = ''
-	def get_folder(self,directory,num):
+
+	def get_folder(self,num):
 		
-		folders = os.listdir(directory)
-		folders = sorted(folders)
+		folders = os.listdir(self.directory)
+		dirs = []
+		for fold in folders:
+			if os.path.isdir(os.path.join(self.directory,fold)):
+				dirs.append(fold)
+		dirs = sorted(dirs)
+		print(dirs)
+		return os.path.join(self.directory,dirs[num-1]) #Folder number starts from 1 (1~5)
 
-		return os.path.join(directory,folders[num])
-
-	def get_file(self,directory,num):
-		files = os.listdir(directory)
+	def get_file(self,folder,num):
+		files = os.listdir(folder)
 		files = sorted(files)
+		return os.path.join(folder,files[num]) #File num starts from 0
 
-		return os.path.join(directory,files[num])
-
-	def open_csv(self,folder,file):
+	def open_csv(self,folder,file): #Return (Review_text,Rating) Tuple List
 		tuples = []
-		self.folder = self.get_folder(self.directory,folder)
+		self.folder = self.get_folder(folder)
 		self.file =  self.get_file(self.folder,file)
 		f = open(self.file,'r',encoding='utf-8')
 		rdr = csv.reader(f)
@@ -30,5 +34,33 @@ class Reader():
 		f.close()
 		return tuples
 
+class InteractiveReader(Reader): 
+
+	def __init__(self):
+		self.directory = os.getcwd()
+		self.folder = ''
+		self.file = ''
+
+		n = input('\n\npress y to continue : ')
+		while n =='y':
+			tuples = []
+			self.folder = self.get_folder(int(input("Choose folder number(1~5) : ")))
+			self.file = self.get_file(self.folder,int(input("Choose file number(1~) : ")))
+			f = open(self.file,'r',encoding='utf-8')
+			rdr = csv.reader(f)
+			for line in rdr:
+				tuples.append(line)
+			f.close()
+			print(tuples)
+			n = input('\n\npress y to continue : ')
+"""
+1. Datasets 폴터 가장 상단에 놓고 사용 
+2. 폴더번호는 1번, 파일번호는 0번 부터 시작
+
+#csvReader Usage
 rdr = Reader()
-print(rdr.open_csv(5,1))
+print(rdr.open_csv(3,1))
+
+#interactiveReader() Usage
+rdr = InteractiveReader()
+"""
