ba3b8f961c0579f10a50cc34ac09e024d6a35939
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-05-10 23:25:51 +0900

    style: add linter

--- PreferenceDiscriminator.py
+++ PreferenceDiscriminator.py
@@ -10,9 +10,11 @@ stemmer = LancasterStemmer()
 
 ######### Helper Functions ########
 
+
 def synset_name(synset):
     """ function to change synset into string """
-    return synset.name().split('.')[0]
+    return synset.name().split(".")[0]
+
 
 def find_wn_synset(word, pos):
     """ function to find wordnet synset with input word and POS """
@@ -24,26 +26,33 @@ def find_wn_synset(word, pos):
         return None
     return synsets[0]
 
+
 def check_word(word):
     """ function to check if the word is normal word """
     if not word.isalpha():
-        if not (word.endswith(',') or word.endswith('.')) and not word[:len(word)-2].isalpha():
+        if (
+            not (word.endswith(",") or word.endswith("."))
+            and not word[: len(word) - 2].isalpha()
+        ):
             return False
     return True
 
+
 def tag_convert(tag):
     """ function to convert nltk.tag to wordnet tag """
-    if tag in ['RB', 'RBR', 'RBS']:
-        return 'r'
-    elif tag in ['JJ', 'JJR', 'JJS']:
-        return 'a'
-    elif tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
-        return 'v'
-    elif tag.startswith('N'):
-        return 'n'
+    if tag in ["RB", "RBR", "RBS"]:
+        return "r"
+    elif tag in ["JJ", "JJR", "JJS"]:
+        return "a"
+    elif tag in ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]:
+        return "v"
+    elif tag.startswith("N"):
+        return "n"
     return None
 
+
 ######### Main Functions #########
+
 
 def get_sentiment(word, tag):
     """ returns (pos, neg, obj) score of input word. Returns None if error """
@@ -55,10 +64,14 @@ def get_sentiment(word, tag):
         return None
     # Take the first sense, the most common
     swn_synset = swn.senti_synset(wn_synset.name())
-    return [swn_synset.pos_score()**2, swn_synset.neg_score()**2, swn_synset.obj_score()**2]
+    return [
+        swn_synset.pos_score() ** 2,
+        swn_synset.neg_score() ** 2,
+        swn_synset.obj_score() ** 2,
+    ]
 
 
-#original version without any additional methods
+# original version without any additional methods
 def get_score_org(review):
     """ Calculate own score by tokenized words' negativity/positivity """
     # Initialize scores
@@ -69,24 +82,24 @@ def get_score_org(review):
     for tagged_word in tagged_word_list:
         if not check_word(tagged_word[0]):
             continue
-        if tagged_word[1].startswith('V'):
+        if tagged_word[1].startswith("V"):
             lemmatized_word = stemmer.stem(tagged_word[0])
             new_score = get_sentiment(lemmatized_word, tagged_word[1])
         else:
             new_score = get_sentiment(tagged_word[0], tagged_word[1])
 
-        if not new_score is None:           
-            for i in range (2):
+        if not new_score is None:
+            for i in range(2):
                 score[i] += new_score[i]
             if score[0] != 0 or score[1] != 0:
                 cnt += 1
     if cnt == 0:
         return [0, 0]
     else:
-        return [s*100/cnt for s in score]
+        return [s * 100 / cnt for s in score]
 
 
-#*5 scores in last two stc.
+# *5 scores in last two stc.
 def get_score(review):
     """ Calculate own score by tokenized words' negativity/positivity """
     # Initialize scores
@@ -98,15 +111,15 @@ def get_score(review):
         for tagged_word in tagged_word_list:
             if not check_word(tagged_word[0]):
                 continue
-            if tagged_word[1].startswith('V'):
+            if tagged_word[1].startswith("V"):
                 lemmatized_word = stemmer.stem(tagged_word[0])
                 new_score = get_sentiment(lemmatized_word, tagged_word[1])
             else:
                 new_score = get_sentiment(tagged_word[0], tagged_word[1])
 
-            if not new_score is None:           
-                for i in range (2):
-                    if sent == (len(words) - 1) or sent == (len(words) -2):
+            if not new_score is None:
+                for i in range(2):
+                    if sent == (len(words) - 1) or sent == (len(words) - 2):
                         score[i] *= 5
                     score[i] += new_score[i]
                 if score[0] != 0 or score[1] != 0:
@@ -114,7 +127,7 @@ def get_score(review):
     if cnt == 0:
         return [0, 0]
     else:
-        return [s*100/cnt for s in score]
+        return [s * 100 / cnt for s in score]
 
 
 def get_score_with_neg_check(review):
@@ -122,23 +135,21 @@ def get_score_with_neg_check(review):
     # Initialize scores
     score = [0, 0]
     cnt = 0
-
     neg_check = 0
-
     tagged_word_list = nltk.pos_tag(nltk.word_tokenize(review))
     for tagged_word in tagged_word_list:
         if not check_word(tagged_word[0]):
             continue
-        if tagged_word[1].startswith('V'):
+        if tagged_word[1].startswith("V"):
             lemmatized_word = stemmer.stem(tagged_word[0])
             new_score = get_sentiment(lemmatized_word, tagged_word[1])
         else:
             new_score = get_sentiment(tagged_word[0], tagged_word[1])
 
-        if not new_score is None:           
+        if not new_score is None:
             if neg_check == 1:
                 new_score[0], new_score[1] = new_score[1], new_score[0]
-            for i in range (2):
+            for i in range(2):
                 score[i] += new_score[i]
             if score[0] != 0 or score[1] != 0:
                 cnt += 1
@@ -150,7 +161,7 @@ def get_score_with_neg_check(review):
     if cnt == 0:
         return [0, 0]
     else:
-        return [s*100/cnt for s in score]
+        return [s * 100 / cnt for s in score]
 
 
 def rate_five(score):
@@ -158,7 +169,7 @@ def rate_five(score):
     if score[0] == 0 and score[1] == 0:
         return 2.5
     else:
-        return 5* score[0]/(score[0]+score[1])
+        return 5 * score[0] / (score[0] + score[1])
 
 
 def csv_write(file_name, pairs_list):
@@ -166,16 +177,16 @@ def csv_write(file_name, pairs_list):
     csv_file = open(file_name, "w")
 
     # header of the table #
-    w = 'Rate, Score-1, Score-2'
+    w = "Rate, Score-1, Score-2"
     csv_file.write(w)
-    csv_file.write('\n')
+    csv_file.write("\n")
 
     # print out 100 results #
     for i in range(len(pairs_list)):
         pair = pairs_list[i]
         w = pair[0] + "," + pair[1] + "," + pair[2]
         csv_file.write(w)
-        csv_file.write('\n')
+        csv_file.write("\n")
     csv_file.close()
 
 
@@ -195,6 +206,8 @@ for line in lines:
     tot2 += abs(float(res2) - float(ans))
     result_list.append([ans, res, res2])
 
-print("original_ver: " + str(tot1/len(lines)) + "add_neg_ver: " + str(tot2/len(lines)))
+print(
+    "original_ver: " + str(tot1 / len(lines)) + "add_neg_ver: " + str(tot2 / len(lines))
+)
 
 csv_write("scoring_result.csv", result_list)
